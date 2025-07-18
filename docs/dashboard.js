af0091b624138e231cfc948c10183e8dfb192544
Nigel Leck <nigel@stSoftware.com.au>
2025-07-18 12:25:20 +1000

    Remove commented-out mobile device badge rendering in dashboard.js

--- docs/dashboard.js
+++ docs/dashboard.js
@@ -261,7 +261,6 @@ function createHostCard(hostname, data) {
             </div>
         </div>
     `;
-                // ${data.mobile ? `<div class="text-center mt-3"><span class="badge bg-warning text-dark"><i class="bi bi-phone"></i> Mobile Device</span></div>` : ''}
     }
 
 function updateStats(hosts) {
