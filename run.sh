fea938bee88a032b0b079e100a1f83e79da57aa0
Nigel Leck <nigel@stSoftware.com.au>
2025-10-07 22:18:56 +1100

    Update version number in run.sh from 1.0.24 to 1.0.25

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ cd "${BASE_DIR}"
 # Configuration
 JSON_FILE="docs/index.json"
 HEARTBEAT_THRESHOLD_HOURS=6
-VERSION="1.0.24"
+VERSION="1.0.25"
 
 # Parse command line arguments
 FORCE_UPDATE=false
