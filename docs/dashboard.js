a7b68e1a01979f69ca223019d8bcabcbb8deb195
Nigel Leck <nigel@stSoftware.com.au>
2025-10-13 14:10:04 +1100

    Update version to 1.0.30 across all relevant files

--- docs/dashboard.js
+++ docs/dashboard.js
@@ -1,5 +1,5 @@
 // Version constant - this will be updated by the git hook
-const VERSION = "1.0.29";
+const VERSION = "1.0.30";
 
 // Set page title with version
 document.title = `GRQ Health Dashboard v${VERSION}`;
