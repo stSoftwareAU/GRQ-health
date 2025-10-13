9ca5cb08882d7d48821cc854482fe69f97ddb221
Nigel Leck <nigel@stSoftware.com.au>
2025-10-13 16:17:00 +1100

    Update version to 1.0.31 in run.sh

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ cd "${BASE_DIR}"
 # Configuration
 JSON_FILE="docs/index.json"
 HEARTBEAT_THRESHOLD_HOURS=6
-VERSION="1.0.30"
+VERSION="1.0.31"
 
 # Parse command line arguments
 FORCE_UPDATE=false
