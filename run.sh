bd5a74a762971c4e10d5feed74d82dd978898041
Nigel Leck <nigel@stsoftware.com.au>
2025-10-21 12:26:19 +1100

    Update version to 1.0.43 and enhance stack trace detection in README and log viewer

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ cd "${BASE_DIR}"
 # Configuration
 JSON_FILE="docs/index.json"
 HEARTBEAT_THRESHOLD_HOURS=6
-VERSION="1.0.42"
+VERSION="1.0.43"
 
 # Parse command line arguments
 FORCE_UPDATE=false
