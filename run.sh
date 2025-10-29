579fbf850e54b4407dcabdbd02f93bb0465f5923
Nigel Leck <nigel@stsoftware.com.au>
2025-10-29 21:27:54 +1100

    Update version to 1.0.49 across all relevant files

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ cd "${BASE_DIR}"
 # Configuration
 JSON_FILE="docs/index.json"
 HEARTBEAT_THRESHOLD_HOURS=6
-VERSION="1.0.48"
+VERSION="1.0.49"
 
 # Parse command line arguments
 FORCE_UPDATE=false
