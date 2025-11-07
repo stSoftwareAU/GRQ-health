7e96c5d87e48fc91cbfc71b5c2d89a15eabd0c18
Nigel Leck <nigel+github@stsoftware.com.au>
2025-11-07 17:23:16 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ cd "${BASE_DIR}"
 # Configuration
 JSON_FILE="docs/index.json"
 HEARTBEAT_THRESHOLD_HOURS=8
-VERSION="1.0.51"
+VERSION="1.0.52"
 
 # Parse command line arguments
 FORCE_UPDATE=false
